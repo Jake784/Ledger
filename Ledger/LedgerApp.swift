4b153a9054e6c127d9f0d537e377abf60f99abbf
Jake784 <callejas2025@gmail.com>
2026-08-15 11:31:59 -0600

    Initial Commit

---
+++ Ledger/LedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedgerApp.swift
+//  Ledger
+//
+//  Created by Luis Callejas on 15/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
